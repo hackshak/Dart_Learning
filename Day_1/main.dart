29f5f36ed6dde4367f8177331c1f1f647df75885
hackshak <msrehman139@gmail.com>
2026-01-12 22:16:29 +0500

    Day 1 completed of learning Dart as i learned about comments,print statement and operators

--- Day_1/main.dart
+++ Day_1/main.dart
@@ -1,3 +1,17 @@
+// This function is main entry point of every dart application 
 void main(){
-    
+
+ print('Day 1 of learning dar');
+
+ print(3);
 }
+
+
+
+// this is single line comment
+
+
+/*
+thi is the multi line
+comment in dart language.
+*/
